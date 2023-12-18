2169c7eca5f73753cd65f6b0718d99027b695040
c1138 <40825968+c1138@users.noreply.github.com>
2023-12-18 20:20:19 +0300

    commit003

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+#App.vue
+<template>
+  <div id="app">
+    <HeaderComponent
+      v-if="$route.name != 'login' && $route.name != '404'"
+    ></HeaderComponent>
+    <router-view></router-view>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/layout/HeaderComponent";
+// import HelloWorld from "./components/HelloWorld.vue";
+import "@/assets/style/global.scss";
+
+export default {
+  name: "app",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style></style>
